ed324d2f9ef145c98eab0c04536f9d19bfa96cd0
Xernical <37638790+Xernical@users.noreply.github.com>
2021-08-21 00:57:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const clientId = '8efe66c9553141b682d78450628421a1';
 const clientSecret = '4ccb23d3318f467ab1faec5a6a03cb42';
 
+let playlistId = 0;
 
 function parsePlaylist() {
   playlistURL = document.getElementById("myText").value;
