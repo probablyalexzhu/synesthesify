c6a8edd2ddf8adec09ef26c49e7bf15f76133d21
yimango <48071660+yimango@users.noreply.github.com>
2021-08-21 09:40:33 -0400

    Update draw.js

--- draw.js
+++ draw.js
@@ -1,6 +1,9 @@
 var image = document.querySelector("img");
 var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext("2d");
+let songsFile = require('./data.json');
+var songs = [];
+
 
 function copyImageToCanvas() {
     
@@ -11,12 +14,49 @@ function location(){
 }
 // where arr is an array containing all the yet to use songs
 function remaining(arr){
+    const coordarray = getSectionCoordinates(arr.length);
     for (var i = 0; i<=arr.length; i++){
-        let x = Math.floor(Math.random()*800);
-        let y = Math.floor(Math.random()*800);
-        draw(arr[i], x, y);
+        draw(arr[i], coordarray[i]);
     }
 }
-function draw(song, x, y){
+function draw(song, coords){
 
 }
+
+const canvasSize = 500;
+
+function randomNumber(size){
+    const h = Math.random()*size+1;
+    return Math.floor(h);
+}
+class coordinate{
+    constructor(x,y){
+        this.x=x; this.y=y;
+    }
+}
+
+class rectangle{
+    //tl is top left, br = bottom right
+    constructor (a,b,c,d){
+        this.tl = new coordinate(a,b);
+        this.br = new coordinate(c,d);
+    }
+}
+function getSectionCoordinates(numberOfSongs){
+    const sqr = Math.floor(Math.sqrt(numberOfSongs))
+    const size = Math.floor(canvasSize/sqr);
+    const leftover = numberOfSongs - (sqr*sqr)
+    arr = [];
+    for(i=0;i+size<=canvasSize;i+=size){
+        for(j=0;j+size<=canvasSize;j+=size){
+            arr.push(new rectangle(i,j,i+size,j+size))
+        }
+    }
+    for(i=0;i<leftover;i++){
+        let randomx = randomNumber(canvasSize-size)-1;
+        let randomy = randomNumber(canvasSize-size)-1;
+        arr.push(new rectangle(randomx,randomy,randomx+size,randomy=size));
+    }
+    console.log(arr);
+}
+getSectionCoordinates(18)
