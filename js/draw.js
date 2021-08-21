4f815a9e390f8ddd98c13f1a16adac2c6911b795
Gavin Song <86136270+gavin-St@users.noreply.github.com>
2021-08-21 18:20:39 -0400

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -1,3 +1,5 @@
+function hugeApp(){
+
 //important 
 //var image = document.querySelector("img");
 var canvas = document.querySelector("#canvas");
@@ -37,23 +39,38 @@ class songData{
     }
 }
 class rgb{
-    constructor(r,b,g){
+    constructor(r,g,b){
         this.r=r; this.b=b; this.g=g;
     }
 }
 
 
-//methods
+//helper methods
 function randomNumber(size){
     const h = Math.random()*size+1;
     return Math.floor(h);
 }
+function numsToRGB(r,g,b){
+    if(r<0) r=0;
+    if(g<0) g=0;
+    if(b<0) b=0;
+    if(r>255) r=255;
+    if(g>255) g=255;
+    if(b>255) b=255;
+    return "rgb("+r.toString()+","+g.toString()+","+b.toString()+")"
+}
 
+//run functions
+let avg_tempo = 0;
+getSongArray();
+drawFromArray();
+//main functions
 
 function getSongArray(){
     for(let i= 0;i<songFeaturesArray.length;i++){
         let obj = songFeaturesArray[i];
         let tempo = obj.tempo;
+        avg_tempo += tempo;
         let a;
         switch(tempo){
             case tempo >= 160:
@@ -130,6 +147,7 @@ function getSongArray(){
         
         songs[i] = new songData(a,b,c,d,e,f)
     }
+    avg_tempo/=songFeaturesArray.length;
 }
 
 function getSectionCoordinates(numberOfSongs){
@@ -151,41 +169,159 @@ function getSectionCoordinates(numberOfSongs){
     return arr;
 }
 
-// where arr is an array containing all the yet to use songs
+// goes through songs[], driver code for all drawing methods
 function drawFromArray(){
-    const coordarray = getSectionCoordinates(songs.length);
-    for (var i = 0; i<=songs.length; i++){
-        draw(songs[i], coordarray[i]);
+    const coordArray = getSectionCoordinates(songs.length);
+    console.log(coordArray[0].tl);
+    drawBackground(avg_tempo);
+    for (var i = 0; i<songs.length; i++){
+        draw(songs[i], coordArray[i]);
     }
 }
+function drawBackground(avg_tempo){
+    let a;
+    switch(avg_tempo){
+        case avg_tempo >= 160:
+            a = new rgb(240, 10 , 10);
+            break;
+        case avg_tempo >= 150:
+            a = new rgb (245, 137, 10);
+            break;
+        case avg_tempo >= 140:
+            a = new rgb (245, 245, 10);
+            break;
+        case avg_tempo >= 130:
+            a = new rgb(20, 240, 15);
+            break;
+        case avg_tempo >= 120:
+            a = new rgb(20, 180, 15);
+            break;
+        case avg_tempo >= 110:
+            a = new rgb(15, 210, 220);
+            break;
+        case avg_tempo >= 100:
+            a = new rgb(15, 20, 224);
+            break;
+        case avg_tempo >= 90:
+            a = new rgb(100, 30, 120);
+            break;
+        default:
+            a = new rgb(120, 80, 30);
+            break;
+    }
+    let grd = ctx.createRadialGradient(250,250,0,250,250,350);
+    grd.addColorStop(0.6,numsToRGB(a.r,a.g,a.b));
+    console.log(avg_tempo);
+    grd.addColorStop(1,"white");
+    ctx.fillStyle = grd;
+    ctx.fillRect(0,0,500,500)
+}
 function draw(song, coords){
-    let abst = song.abstractness;
-    let shape_count;
     let tl = coords.tl;
     let br = coords.br;
-    switch(abst){
-
+    //opacity -- set opacity, change stroke size
+    ctx.globalAlpha=song.opacity;
+    //DO STROKE WIDTH!!!!!!!!!!!!!
+    //hue -- get base color
+    let cur_rgb  = song.hue;
+    //brightness -- change the hue by certain amount, add randomness
+    let bright = song.brightness;
+    let r1 = randomNumber(6)-4; let r2 = randomNumber(6)-4; let r3 = randomNumber(6)-4;
+    cur_rgb.r+=bright-10+r1;
+    cur_rgb.g+=bright-10+r2;
+    cur_rgb.b+=bright-10+r3;
+    //CHANGE COLLOR OF STROKE!!!!!!!!!
+    //texture -- fill it in with gradient
+    let r4 = randomNumber(4);
+    switch(r4){
+        case 4:
+            grad = ctx.createLinearGradient(tl.x,tl.y,br.x,br.y);
+            grad.addColorStop(0,numsToRGB(cur_rgb.r-15,cur_rgb.g-15,cur_rgb.b-15));
+            grad.addColorStop(1,numsToRGB(cur_rgb.r+15,cur_rgb.g+15,cur_rgb.b+15));
+            ctx.fillStyle = grad;
+            ctx.fillRect(tl.x,tl.y,br.x,br.y);
+            break;
+        case 3:
+            grad = ctx.createLinearGradient(br.x,br.y,tl.x,tl.y);
+            grad.addColorStop(0,numsToRGB(cur_rgb.r-15,cur_rgb.g-15,cur_rgb.b-15));
+            grad.addColorStop(1,numsToRGB(cur_rgb.r+15,cur_rgb.g+15,cur_rgb.b+15));
+            ctx.fillStyle = grad;
+            ctx.fillRect(br.x,br.y,tl.x,tl.y);
+            break;
+        case 2:
+            grad = ctx.createLinearGradient(br.x,tl.y,tl.x,br.y);
+            grad.addColorStop(0,numsToRGB(cur_rgb.r-15,cur_rgb.g-15,cur_rgb.b-15));
+            grad.addColorStop(1,numsToRGB(cur_rgb.r+15,cur_rgb.g+15,cur_rgb.b+15));
+            ctx.fillStyle = grad;
+            ctx.fillRect(br.x,tl.y,tl.x,br.y);
+            break;
+        case 1:
+            grad = ctx.createLinearGradient(tl.x,br.y,br.x,tl.y);
+            grad.addColorStop(0,numsToRGB(cur_rgb.r-15,cur_rgb.g-15,cur_rgb.b-15));
+            grad.addColorStop(1,numsToRGB(cur_rgb.r+15,cur_rgb.g+15,cur_rgb.b+15));
+            ctx.fillStyle = grad;
+            ctx.fillRect(tl.x,br.y,br.x,tl.y);
+            break;
     }
-        
+
+    //abstractness -- get shape count and possibly get image
+    let abst = song.abstractness;
+    let shape_count= 0;
+    
+    switch(abst){
+        case 3:
+            shape_count=15;
+            break;
+        case 2:
+            shape_count = 10;
+            break;
+        case 1:
+            shape_count = 5;
+            copyImageToCanvas(); break;
+        case 0:
+            copyImageToCanvas(); break; 
+    }
+    //energy -- draw the shapes --cricles,rectangles,lines
     let energy = song.energy;
     switch(energy){
         case 3:
             for (let i = 0; i < shape_count; i++){
                 ctx.beginPath();
-                let centerx = randomNumber(section_size-2);
-                let centery = randomNumber(section_size-2)
+                let centerx = randomNumber(section_size-2)+ tl.x;
+                let centery = randomNumber(section_size-2)+ tl.y;
                 let rad = randomNumber(section_size/2);
-                ctx.arc(centerx + tl.x, centery+ tl.y, rad, 0, 2*Math.PI);
+                ctx.arc(centerx , centery, rad, 0, 2*Math.PI);
                 ctx.stroke();
             }
             break;
 
         case 2:
-            for (let i = )
-//stuff to do: stroke colour, stroke width 
-    }
+            for (let i = 0; i < shape_count; i++){
+                let centerx = randomNumber(section_size-2)+tl.x;
+                let centery = randomNumber(section_size-2) + tl.y;
+                let len = randomNumber(section_size-2);
+                let width = randomNumber(section_size-2);
+                ctx.rect(centeerx,centery,centerx+len,centery+width);
+            }
+            break;
+        case 1:
+            for (let i = 0; i < shape_count; i++){
+                let firstx = randomNumber(section_size-2)+tl.x;
+                let firsty = randomNumber(section_size-2) + tl.y;
+                let secondx = randomNumber(section_size-2)+tl.x;
+                let secondy = randomNumber(section_size-2)+tl.y;
+                ctx.beginPath();
+                ctx.moveTo(firstx,firsty);
+                ctx.lineTo(secondx,secondy);
+                ctx.stroke();
+            }
+            break;
+    }//end switch
     
 }
 function copyImageToCanvas() {
     
+}
+
+
 }
