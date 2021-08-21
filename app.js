e8d779931b554688008dee64631b301106560194
Xernical <37638790+Xernical@users.noreply.github.com>
2021-08-21 00:26:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const clientId = '8efe66c9553141b682d78450628421a1'
-const clientSecret = '4ccb23d3318f467ab1faec5a6a03cb42'
+const clientId = '8efe66c9553141b682d78450628421a1';
+const clientSecret = '4ccb23d3318f467ab1faec5a6a03cb42';
 
+let playlistId = 0;
 
 function parsePlaylist() {
   playlistURL = document.getElementById("myText").value;
@@ -18,31 +19,16 @@ function parsePlaylist() {
   //https://open.spotify.com/playlist/2UEOgAtDT49WHsYzYew65f?si=ab5ad01c816a48c1
 }
 
-const _getToken = async () => {
+let request = require("request");
+let token = "Bearer ";
+let playlist_request = "https://api.spotify.com/v1/playlists/" + playlistId;
 
-        const result = await fetch('https://accounts.spotify.com/api/token', {
-            method: 'POST',
-            headers: {
-                'Content-Type' : 'application/x-www-form-urlencoded', 
-                'Authorization' : 'Basic ' + btoa(clientId + ':' + clientSecret)
-            },
-            body: 'grant_type=client_credentials'
-        });
-
-        const data = await result.json();
-        return data.access_token;
-    }
-//https://api.spotify.com/v1/playlists/{playlist_id}
-
-const _getPlaylists = async () => {
-
-        const result = await fetch('https://api.spotify.com/v1/playlists/' + playlistId, {
-            method: 'GET',
-            headers: { 'Authorization' : 'Bearer ' + 'BQDwP4jRgyh1MH_XijiGJSsp1u0kRTOGGggcUHmYVM38ckP3PzK5CWDRakb8cQGaXR1Wuzh5338QaFtN-AJDKBz3lEW2rF4UW-0_Y1JYbOU9xN9vZaV-REjpkEda84EDRXZfFOzAHfyuup7qSOht12qLIfM'}
-        });
-
-        const data = await result.json();
-        document.getElementById("result").innerHTML = data
-        return data.categories.items;
-        
-    }
+request({url:playlist_request, headers:{"Authorization":token}}, function(err, res) {
+  if (res) {
+    let songs = JSON.parse(res.body);
+    console.log("song: " + songs.name;
+    songs.tracks.forEach (function(track) {
+      console.log(track.track.name);
+    });
+  }
+})
