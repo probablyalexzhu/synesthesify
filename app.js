6f405d2642afc4a74837246861eb0a5fed63f082
Xernical <37638790+Xernical@users.noreply.github.com>
2021-08-20 22:27:52 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const clientId = 8efe66c9553141b682d78450628421a1
-const clientSecret = 4ccb23d3318f467ab1faec5a6a03cb42
+const clientId = 8efe66c9553141b682d78450628421a1;
+const clientSecret = 4ccb23d3318f467ab1faec5a6a03cb42;
 
 
 function parsePlaylist() {
