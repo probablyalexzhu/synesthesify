e0eb1532ee1a52ab65012fe2e73b31f436ba0268
yimango <48071660+yimango@users.noreply.github.com>
2021-08-21 09:16:37 -0400

    Update draw.js

--- draw.js
+++ draw.js
@@ -9,3 +9,14 @@ function copyImageToCanvas() {
 function location(){
 
 }
+// where arr is an array containing all the yet to use songs
+function remaining(arr){
+    for (var i = 0; i<=arr.length; i++){
+        let x = Math.floor(Math.random()*800);
+        let y = Math.floor(Math.random()*800);
+        draw(arr[i], x, y);
+    }
+}
+function draw(song, x, y){
+
+}
