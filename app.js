f5089359b4e749d67d7c9dbdfa8e24364cbe04d3
Alex Zhu <87958079+probablyalexzhu@users.noreply.github.com>
2021-08-20 23:45:03 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
+// david's code
+function parsePlaylist() {
+    playlistURL = document.getElementById("myText").value;
+    if (playlistURL.includes("https://open.spotify.com/playlist/")) {
+      playlistId = playlistURL.split("/");
+      playlistId = playlistId[4].split("?");
+      document.getElementById("playlist-id").innerHTML = playlistId[0];
+    } else if (playlistURL.includes("https://open.spotify.com/user/")) {
+      playlistId = playlistURL.split("/");
+      playlistId = playlistId[6].split("?");
+      document.getElementById("playlist-id").innerHTML = playlistId[0];
+    } else {
+      document.getElementById("playlist-id").innerHTML = "You entered an invalid playlist URL!";
+    }
+    //https://open.spotify.com/playlist/2UEOgAtDT49WHsYzYew65f?si=ab5ad01c816a48c1
+  }
+
 const APIController = (function() {
     
     const clientId = '8efe66c9553141b682d78450628421a1'
     const clientSecret = '4ccb23d3318f467ab1faec5a6a03cb42'
-
-    // david's code
-    function parsePlaylist() {
-        playlistURL = document.getElementById("myText").value;
-        if (playlistURL.includes("https://open.spotify.com/playlist/")) {
-          playlistId = playlistURL.split("/");
-          playlistId = playlistId[4].split("?");
-          document.getElementById("playlist-id").innerHTML = playlistId[0];
-        } else if (playlistURL.includes("https://open.spotify.com/user/")) {
-          playlistId = playlistURL.split("/");
-          playlistId = playlistId[6].split("?");
-          document.getElementById("playlist-id").innerHTML = playlistId[0];
-        } else {
-          document.getElementById("playlist-id").innerHTML = "You entered an invalid playlist URL!";
-        }
-        //https://open.spotify.com/playlist/2UEOgAtDT49WHsYzYew65f?si=ab5ad01c816a48c1
-      }
 
     // private methods
     const _getToken = async () => {
