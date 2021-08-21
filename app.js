751ab036986b5f602d7d9cec49cd3762b9d9640f
Xernical <37638790+Xernical@users.noreply.github.com>
2021-08-20 22:25:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 const clientId = 8efe66c9553141b682d78450628421a1
 const clientSecret = 4ccb23d3318f467ab1faec5a6a03cb42
+
+
+function parsePlaylist() {
+  playlistURL = document.getElementById("myText").value;
+  if playlistURL.includes("https://open.spotify.com/playlist/") {
+    playlistId = playlistURL.split("/");
+    playlistId = playlistId[4].split("?");
+    document.getElementById("playlist-id").innerHTML = playlistId[0];
+  } else {
+    document.getElementById("playlist-id").innerHTML = "You entered an invalid playlist URL!"
+  }
+  //https://open.spotify.com/playlist/2UEOgAtDT49WHsYzYew65f?si=ab5ad01c816a48c1
+}
