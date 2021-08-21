5a35f2157e42cc4dac08dcbac7670f4586dfb3df
yimango <48071660+yimango@users.noreply.github.com>
2021-08-21 09:00:47 -0400

    Add files via upload

---
+++ draw.js
@@ -0,0 +1,11 @@
+var image = document.querySelector("img");
+var canvas = document.querySelector("canvas");
+var ctx = canvas.getContext("2d");
+
+function copyImageToCanvas() {
+    
+}
+
+function location(){
+
+}
