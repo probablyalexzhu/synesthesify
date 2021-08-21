49d3c60a170e7156c8583bbd32f2d27e06f63693
Alex Zhu <87958079+probablyalexzhu@users.noreply.github.com>
2021-08-21 00:15:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const clientId = 8efe66c9553141b682d78450628421a1;
-const clientSecret = 4ccb23d3318f467ab1faec5a6a03cb42;
+const clientId = '8efe66c9553141b682d78450628421a1'
+const clientSecret = '4ccb23d3318f467ab1faec5a6a03cb42'
 
 
 function parsePlaylist() {
   playlistURL = document.getElementById("myText").value;
-  if (playlistURL.includes("https://open.spotify.com/playlist/%22)) {
+  if (playlistURL.includes("https://open.spotify.com/playlist/")) {
     playlistId = playlistURL.split("/");
     playlistId = playlistId[4].split("?");
     document.getElementById("playlist-id").innerHTML = playlistId[0];
-  } else if (playlistURL.includes("https://open.spotify.com/user/%22)) {
+  } else if (playlistURL.includes("https://open.spotify.com/user/")) {
     playlistId = playlistURL.split("/");
     playlistId = playlistId[6].split("?");
     document.getElementById("playlist-id").innerHTML = playlistId[0];
@@ -32,17 +32,17 @@ const _getToken = async () => {
         const data = await result.json();
         return data.access_token;
     }
-//https://api.spotify.com/v1/playlists/%7Bplaylist_id%7D
+//https://api.spotify.com/v1/playlists/{playlist_id}
 
-const _getPlaylists = async (token) => {
+const _getPlaylists = async () => {
 
-        const result = await fetch('https://api.spotify.com/v1/playlists/$%7Bplaylist_id%7D', {
+        const result = await fetch('https://api.spotify.com/v1/playlists/' + playlistId, {
             method: 'GET',
-            headers: { 'Authorization' : 'Bearer ' + token}
+            headers: { 'Authorization' : 'Bearer ' + 'BQDwP4jRgyh1MH_XijiGJSsp1u0kRTOGGggcUHmYVM38ckP3PzK5CWDRakb8cQGaXR1Wuzh5338QaFtN-AJDKBz3lEW2rF4UW-0_Y1JYbOU9xN9vZaV-REjpkEda84EDRXZfFOzAHfyuup7qSOht12qLIfM'}
         });
 
         const data = await result.json();
         document.getElementById("result").innerHTML = data
         return data.categories.items;
-
+        
     }
