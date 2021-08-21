aa3c515330b4a6ea180969210e97b84b9a54a3cd
Xernical <37638790+Xernical@users.noreply.github.com>
2021-08-20 23:32:43 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-//const clientId = 8efe66c9553141b682d78450628421a1;
-//const clientSecret = 4ccb23d3318f467ab1faec5a6a03cb42;
+const clientId = 8efe66c9553141b682d78450628421a1;
+const clientSecret = 4ccb23d3318f467ab1faec5a6a03cb42;
 
 
 function parsePlaylist() {
@@ -17,3 +17,32 @@ function parsePlaylist() {
   }
   //https://open.spotify.com/playlist/2UEOgAtDT49WHsYzYew65f?si=ab5ad01c816a48c1
 }
+
+const _getToken = async () => {
+
+        const result = await fetch('https://accounts.spotify.com/api/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/x-www-form-urlencoded', 
+                'Authorization' : 'Basic ' + btoa(clientId + ':' + clientSecret)
+            },
+            body: 'grant_type=client_credentials'
+        });
+
+        const data = await result.json();
+        return data.access_token;
+    }
+//https://api.spotify.com/v1/playlists/{playlist_id}
+
+const _getPlaylists = async (token) => {
+
+        const result = await fetch('https://api.spotify.com/v1/playlists/${playlist_id}', {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + token}
+        });
+
+        const data = await result.json();
+        document.getElementById("result").innerHTML = data
+        return data.categories.items;
+        
+    }
