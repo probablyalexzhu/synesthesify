7963689c4830d7a72bf6914ddc222d2f141c3903
David Zu <37638790+Xernical@users.noreply.github.com>
2021-08-21 12:02:11 -0400

    Update app.js

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ function parsePlaylist() {
   let playlistURL = document.getElementById("myText").value; let playlistId=0;
   let embedURL = playlistURL.slice(0,24) + "/embed" + playlistURL.slice(24);
   document.getElementById("embed").src = embedURL;
+  document.getElementById("embed").style.visibility = "visible";
   //console.log(embedURL);
   if (playlistURL.includes("https://open.spotify.com/playlist/")) {
     playlistId = playlistURL.split("/");
