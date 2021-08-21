2430de146eb394c34508a31015480bc53ba2a3e3
Alex Zhu <87958079+probablyalexzhu@users.noreply.github.com>
2021-08-20 23:39:46 -0400

    Add files via upload
    trying template code to make a working version pls

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-const clientId = 8efe66c9553141b682d78450628421a1;
-const clientSecret = 4ccb23d3318f467ab1faec5a6a03cb42;
+const APIController = (function() {
+    
+    const clientId = '8efe66c9553141b682d78450628421a1'
+    const clientSecret = '4ccb23d3318f467ab1faec5a6a03cb42'
 
+    // david's code
+    function parsePlaylist() {
+        playlistURL = document.getElementById("myText").value;
+        if (playlistURL.includes("https://open.spotify.com/playlist/")) {
+          playlistId = playlistURL.split("/");
+          playlistId = playlistId[4].split("?");
+          document.getElementById("playlist-id").innerHTML = playlistId[0];
+        } else if (playlistURL.includes("https://open.spotify.com/user/")) {
+          playlistId = playlistURL.split("/");
+          playlistId = playlistId[6].split("?");
+          document.getElementById("playlist-id").innerHTML = playlistId[0];
+        } else {
+          document.getElementById("playlist-id").innerHTML = "You entered an invalid playlist URL!";
+        }
+        //https://open.spotify.com/playlist/2UEOgAtDT49WHsYzYew65f?si=ab5ad01c816a48c1
+      }
 
-function parsePlaylist() {
-  playlistURL = document.getElementById("myText").value;
-  if (playlistURL.includes("https://open.spotify.com/playlist/")) {
-    playlistId = playlistURL.split("/");
-    playlistId = playlistId[4].split("?");
-    document.getElementById("playlist-id").innerHTML = playlistId[0];
-  } else if (playlistURL.includes("https://open.spotify.com/user/")) {
-    playlistId = playlistURL.split("/");
-    playlistId = playlistId[6].split("?");
-    document.getElementById("playlist-id").innerHTML = playlistId[0];
-  } else {
-    document.getElementById("playlist-id").innerHTML = "You entered an invalid playlist URL!";
-  }
-  //https://open.spotify.com/playlist/2UEOgAtDT49WHsYzYew65f?si=ab5ad01c816a48c1
-}
-
-const _getToken = async () => {
+    // private methods
+    const _getToken = async () => {
 
         const result = await fetch('https://accounts.spotify.com/api/token', {
             method: 'POST',
@@ -32,9 +35,8 @@ const _getToken = async () => {
         const data = await result.json();
         return data.access_token;
     }
-//https://api.spotify.com/v1/playlists/{playlist_id}
-
-const _getPlaylists = async (token) => {
+    
+    const _getPlaylists = async (token) => {
 
         const result = await fetch('https://api.spotify.com/v1/playlists/${playlist_id}', {
             method: 'GET',
@@ -46,3 +48,161 @@ const _getPlaylists = async (token) => {
         return data.categories.items;
         
     }
+
+    return {
+        getToken() {
+            return _getToken();
+        },
+        getPlaylists(token) {
+            return _getGenres(token);
+        }
+    }
+})();
+
+
+// UI Module
+const UIController = (function() {
+
+    //object to hold references to html selectors
+    const DOMElements = {
+        selectGenre: '#select_genre',
+        selectPlaylist: '#select_playlist',
+        buttonSubmit: '#btn_submit',
+        divSongDetail: '#song-detail',
+        hfToken: '#hidden_token',
+        divSonglist: '.song-list'
+    }
+
+    //public methods
+    return {
+
+        //method to get input fields
+        inputField() {
+            return {
+                genre: document.querySelector(DOMElements.selectGenre),
+                playlist: document.querySelector(DOMElements.selectPlaylist),
+                tracks: document.querySelector(DOMElements.divSonglist),
+                submit: document.querySelector(DOMElements.buttonSubmit),
+                songDetail: document.querySelector(DOMElements.divSongDetail)
+            }
+        },
+
+        // need methods to create select list option
+        createGenre(text, value) {
+            const html = `<option value="${value}">${text}</option>`;
+            document.querySelector(DOMElements.selectGenre).insertAdjacentHTML('beforeend', html);
+        }, 
+
+        createPlaylist(text, value) {
+            const html = `<option value="${value}">${text}</option>`;
+            document.querySelector(DOMElements.selectPlaylist).insertAdjacentHTML('beforeend', html);
+        },
+
+        // removed some
+
+        resetTrackDetail() {
+            this.inputField().songDetail.innerHTML = '';
+        },
+
+        resetTracks() {
+            this.inputField().tracks.innerHTML = '';
+            this.resetTrackDetail();
+        },
+
+        resetPlaylist() {
+            this.inputField().playlist.innerHTML = '';
+            this.resetTracks();
+        },
+        
+        storeToken(value) {
+            document.querySelector(DOMElements.hfToken).value = value;
+        },
+
+        getStoredToken() {
+            return {
+                token: document.querySelector(DOMElements.hfToken).value
+            }
+        }
+    }
+
+})();
+
+const APPController = (function(UICtrl, APICtrl) {
+
+    // get input field object ref
+    const DOMInputs = UICtrl.inputField();
+
+    // get genres on page load
+    const loadGenres = async () => {
+        //get the token
+        const token = await APICtrl.getToken();           
+        //store the token onto the page
+        UICtrl.storeToken(token);
+        //get the genres
+        const genres = await APICtrl.getGenres(token);
+        //populate our genres select element
+        genres.forEach(element => UICtrl.createGenre(element.name, element.id));
+    }
+
+    // create genre change event listener
+    DOMInputs.genre.addEventListener('change', async () => {
+        //reset the playlist
+        UICtrl.resetPlaylist();
+        //get the token that's stored on the page
+        const token = UICtrl.getStoredToken().token;        
+        // get the genre select field
+        const genreSelect = UICtrl.inputField().genre;       
+        // get the genre id associated with the selected genre
+        const genreId = genreSelect.options[genreSelect.selectedIndex].value;             
+        // ge the playlist based on a genre
+        const playlist = await APICtrl.getPlaylistByGenre(token, genreId);       
+        // create a playlist list item for every playlist returned
+        playlist.forEach(p => UICtrl.createPlaylist(p.name, p.tracks.href));
+    });
+     
+
+    // create submit button click event listener
+    DOMInputs.submit.addEventListener('click', async (e) => {
+        // prevent page reset
+        e.preventDefault();
+        // clear tracks
+        UICtrl.resetTracks();
+        //get the token
+        const token = UICtrl.getStoredToken().token;        
+        // get the playlist field
+        const playlistSelect = UICtrl.inputField().playlist;
+        // get track endpoint based on the selected playlist
+        const tracksEndPoint = playlistSelect.options[playlistSelect.selectedIndex].value;
+        // get the list of tracks
+        const tracks = await APICtrl.getTracks(token, tracksEndPoint);
+        // create a track list item
+        tracks.forEach(el => UICtrl.createTrack(el.track.href, el.track.name))
+        
+    });
+
+    // create song selection click event listener
+    DOMInputs.tracks.addEventListener('click', async (e) => {
+        // prevent page reset
+        e.preventDefault();
+        UICtrl.resetTrackDetail();
+        // get the token
+        const token = UICtrl.getStoredToken().token;
+        // get the track endpoint
+        const trackEndpoint = e.target.id;
+        //get the track object
+        const track = await APICtrl.getTrack(token, trackEndpoint);
+        // load the track details
+        UICtrl.createTrackDetail(track.album.images[2].url, track.name, track.artists[0].name);
+    });    
+
+    return {
+        init() {
+            console.log('App is starting');
+            loadGenres();
+        }
+    }
+
+})(UIController, APIController);
+
+// will need to call a method to load the genres on page load
+APPController.init();
